4951f98e89c95dcf721524e0c1e016c565c96323
Guzman Monne <guzmonne@hotmail.com>
2018-10-27 22:50:08 -0300

    fixed react-router-dom

--- src/App/EnhancedApp.js
+++ src/App/EnhancedApp.js
@@ -1,11 +1,13 @@
 import compose from 'recompose/compose.js';
 import lifecycle from 'recompose/lifecycle';
+import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import get from 'lodash/get.js';
 import { LOGIN, LOGOUT, RUN } from '../state/actions.js';
 import App from './App.js';
 
 const EnhancedApp = compose(
+  withRouter,
   connect(
     state => ({
       loading: get(state, 'ui.loading.login', false),
