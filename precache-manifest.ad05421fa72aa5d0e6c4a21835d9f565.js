e330a963748c3faf75f725144a45daaccf60cd1c
Guzman Monne <guzmonne@hotmail.com>
2018-11-03 23:02:34 -0300

    Updates

---
+++ precache-manifest.ad05421fa72aa5d0e6c4a21835d9f565.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0d19a27ee0861619c7c",
+    "url": "/awx_frontend/static/js/runtime~main.f0d19a27.js"
+  },
+  {
+    "revision": "efa1cbb252af5e9bc13a",
+    "url": "/awx_frontend/static/js/main.efa1cbb2.chunk.js"
+  },
+  {
+    "revision": "5d70d75257b3632f03c0",
+    "url": "/awx_frontend/static/js/1.5d70d752.chunk.js"
+  },
+  {
+    "revision": "efa1cbb252af5e9bc13a",
+    "url": "/awx_frontend/static/css/main.185261b1.chunk.css"
+  },
+  {
+    "revision": "b14ec7979091781bb5039d550ff99c23",
+    "url": "/awx_frontend/index.html"
+  }
+];
